c8210c8b31bd9e1657a916dd056b42aa3c72e100
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2022-03-25 19:16:56 +0100

    fade in out added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 $('#search-icon').click(function() {
     $('#input-slide').toggle("slide");
   });
+
+  $('#info').click(function() {
+    $('#trailer').animate({'zoom': 0.9, 'opacity': 1}, 400).show();
+  })
+
+  $('#close').click(function() {
+    $('#trailer').animate({'zoom': 0.5, 'opacity': 0}, 400);
+  })
+  
