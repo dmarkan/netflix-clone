7131ad39c1a038a4ce3f5517920401908606ae68
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2022-03-26 13:06:54 +0100

    first slider added

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,13 @@ $('#search-icon').click(function() {
     $('#trailer').animate({'zoom': 0.5, 'opacity': 0}, 400);
   })
   
+  $('#next').click(function() {
+    $('#next-item').animate({'opacity': 0}, 400);
+    $('#prev-item').animate({'opacity': 1}, 400);
+  })
+
+  $('#prev').click(function() {
+    $('#prev-item').animate({'opacity': 0}, 400);
+    $('#next-item').animate({'opacity': 1}, 400);
+  })
+  
