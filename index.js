b05a69bf9bc99cf82e6a40b90e033c2af65d95f5
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2022-03-29 19:24:40 +0200

    site completed

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 })
 
-app.listen(3000, () => {
-    console.log('server is running');
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port 3000");
 })
